76afb32c146e4fa392e7c045d749abba211f18b2
TheDutchMC04 <tobias64a@gmail.com>
2018-05-04 16:55:41 +0200

    added variables

--- src/main/Client.java
+++ src/main/Client.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.io.DataOutputStream;
 import java.net.Socket;
 
 public class Client {
@@ -8,9 +9,13 @@ public class Client {
 		try {
 			System.out.println("Client Started");
 			Socket soc = new Socket("localhost", 9800);
+			DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
 		}
 		catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		private static String videoHeader = "https://www.youtube.com/watch?v=";
+		private static String[] videoIDs;
 	}
 }
