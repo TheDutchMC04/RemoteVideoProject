9dbac1715666ede4c6680ac609d1602195c61984
TheDutchMC04 <tobias64a@gmail.com>
2018-05-04 16:34:29 +0200

    Added try/catch for client+server

--- src/main/Client.java
+++ src/main/Client.java
@@ -1,5 +1,16 @@
 package main;
 
-public class Client {
+import java.net.Socket;
 
+public class Client {
+	
+	public static void main(String[] args) {
+		try {
+			System.out.println("Client Started");
+			Socket soc = new Socket("localhost", 9800);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
