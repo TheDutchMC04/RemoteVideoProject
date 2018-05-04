979f2936b788c4bc71a578313538a719f838ce87
TheDutchMC04 <tobias64a@gmail.com>
2018-05-04 16:40:37 +0200

    added try/catch for server

--- src/main/Server.java
+++ src/main/Server.java
@@ -1,5 +1,23 @@
 package main;
 
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+		
+		public static void main(String[] args) {
+			// TODO: Auto generated method stub
+			
+			try {
+				System.out.println("Waiting for client...");
+				ServerSocket ss = new ServerSocket(9800);
+				Socket soc = ss.accept();
+				System.out.println("Connection Established");
+			}
+			catch (IOException e) {
+				//TODO Auto Generated Catch block
+				e.printStackTrace();
+			}
+		}
 }
